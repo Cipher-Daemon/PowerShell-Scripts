eea993d7730d2b27ecb52127d3be5867177c3b2a
Cipher-Daemon <105097480+Cipher-Daemon@users.noreply.github.com>
2022-07-26 15:20:56 -0500

    Update Get-Blocked-Credential-Users.ps1

--- MS-Exchange-and-365/Get-Blocked-Credential-Users.ps1
+++ MS-Exchange-and-365/Get-Blocked-Credential-Users.ps1
@@ -2,6 +2,9 @@
 $SharedMailboxes = Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -Eq "SharedMailbox"}
 
 #Must connect to tenant first via Connect-MsolService command for following command to work
+
+$DefaultDomainName = Get-AcceptedDomain | Where-Object Default -EQ True
+
 Foreach ($user in $SharedMailboxes) {
 get-MsolUser -UserPrincipalName $user.UserPrincipalName |select UserPrincipalName, blockcredential|where-object {$_.blockcredential -eq "False"}|Export-Csv C:\temp\$DefaultDomainName-BlockedCredential-Not-Enabled-For-SharedMailboxes.csv -Append
 }
