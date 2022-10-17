72cf2d3b121bd02562178244e16ee3bf334ad4d9
Cipher-Daemon <105097480+Cipher-Daemon@users.noreply.github.com>
2022-10-17 16:29:42 -0500

    Version 2
    Script now recognizes if DKIM key is not ready to be rotated.

--- MS-Exchange-and-365/Rotate-All-Keys-to-2048.ps1
+++ MS-Exchange-and-365/Rotate-All-Keys-to-2048.ps1
@@ -3,13 +3,21 @@ $AllDomains = Get-AcceptedDomain
 Foreach ($domain in $AllDomains){
     $SelKeySize1 = (get-DkimSigningConfig -identity $domain.domainname).Selector1KeySize
     $SelKeySize2 = (get-DkimSigningConfig -identity $domain.domainname).Selector2KeySize
+    $RotationDate = (get-DkimSigningConfig -identity $domain.domainname).rotateondate
+    #$RotationDateUTC = $RotationDate.ToUniversalTime()
+    $CurrentDate = Get-date
     if ($SelKeySize1 -eq 1024 -or $SelKeySize2 -eq 1024){
-        Rotate-DkimSigningConfig -KeySize 2048 -identity $domain.domainname
-        write-host -ForegroundColor cyan "$domain key rotated"
-
+        if ($RotationDate.tostring("yyyyMMddHHMM") -le $CurrentDate.tostring("yyyyMMddHHMM")){
+            Rotate-DkimSigningConfig -KeySize 2048 -identity $domain.domainname
+            write-host -ForegroundColor cyan "$domain key rotated"
+        }
+        else{
+            $Days = (New-TimeSpan -start ($CurrentDate).touniversaltime() -End $RotationDate.ToUniversalTime()).days
+            $Hours = (New-TimeSpan -start ($CurrentDate).touniversaltime() -End $RotationDate.ToUniversalTime()).hours
+            $Minutes = (New-TimeSpan -start ($CurrentDate).touniversaltime() -End $RotationDate.ToUniversalTime()).minutes
+            write-host -ForegroundColor red "Key for $Domain not ready to be rotated yet! Please try again in $Days days $Hours hours and $Minutes Minutes."
+        }
     }else {
         write-host -ForegroundColor magenta "$domain already has all keys rotated to 2048 bit, no changes made!"
     }
 }
-
-#If this is the first time running this script, wait 72 hours or more to rotate the second key for full rotation to complete.
