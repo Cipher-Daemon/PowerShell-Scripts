d9868dd481dfa91e686b9ecaad2d116328112d0e
Cipher-Daemon <105097480+Cipher-Daemon@users.noreply.github.com>
2022-10-26 13:43:29 -0500

    Create Move-Computers-From-Text-File.ps1

---
+++ On-Prem-AD/Move-Computers-From-Text-File.ps1
@@ -0,0 +1,17 @@
+#prerequisite: you must have the OU path and Text file available ahead of time prior to writing this script
+
+$FilePath = read-host -prompt "File to read from (Include full file path)."
+$FinalPath = Read-host -prompt "Full OU Path to move machines to (No quotes)."
+$Computers = get-content -path $FilePath
+$ErrorActionPreference= 'silentlycontinue'
+
+foreach ($Computer in $Computers){
+    $Machine = (get-adcomputer -Identity $Computer -errorvariable NoComputer).DistinguishedName
+    if ($NoComputer){
+        write-host -foregroundcolor red "Computer $Computer does not exits!"
+    }else{
+        move-adobject -Identity $Machine -targetpath $FinalPath
+        write-host -ForegroundColor Green "Computer $computer has been moved"
+        }
+    
+}
