6a715455b89ce7063ddc54a9b651c8e4b003015a
Cipher-Daemon <105097480+Cipher-Daemon@users.noreply.github.com>
2022-10-27 10:13:24 -0500

    Create Move-Computers-From-Text-File-Workstations-Only.ps1

---
+++ On-Prem-AD/Move-Computers-From-Text-File-Workstations-Only.ps1
@@ -0,0 +1,24 @@
+#Requires -RunAsAdministrator
+#Prerequisite: you must have the OU path and Text file available ahead of time prior to running this script
+
+$FilePath = read-host -prompt "File to read from (Include full file path)."
+$FinalPath = Read-host -prompt "Full OU Path to move machines to (No quotes)."
+$Computers = get-content -path $FilePath
+$ErrorActionPreference= 'silentlycontinue'
+
+foreach ($Computer in $Computers){
+    $Machine = (get-adcomputer -Identity $Computer -errorvariable NoComputer).DistinguishedName
+    if ($NoComputer){
+        write-host -foregroundcolor red "Computer $Computer does not exist! Skipping."
+    }else{
+        $OSCheck = (get-adcomputer $Machine -properties *|select operatingsystem).operatingsystem
+        if ($OSCheck -notlike '*SERVER*'){
+            move-adobject -Identity $Machine -targetpath $FinalPath
+            write-host -ForegroundColor Green "Computer $computer has been moved!"
+        }else{
+            write-host -foregroundcolor magenta "Computer $Computer is a server, ignoring!"
+        }
+        
+        }
+    
+}
