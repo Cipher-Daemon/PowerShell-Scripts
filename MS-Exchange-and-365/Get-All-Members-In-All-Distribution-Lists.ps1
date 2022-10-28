cebbe3a334e4636c0a80ab5846ba2020cd5f939d
Cipher-Daemon <105097480+Cipher-Daemon@users.noreply.github.com>
2022-10-27 23:09:28 -0500

    Create Get-All-Members-In-All-Distribution-Lists.ps1

---
+++ MS-Exchange-and-365/Get-All-Members-In-All-Distribution-Lists.ps1
@@ -0,0 +1,9 @@
+$EmailGroups = Get-MsolGroup -GroupType DistributionList
+$CompanyName = read-host "What is the Company Name?"
+$Path = c:\temp\$CompanyName\$GroupName-Members.csv
+foreach ($Group in $EmailGroups){
+    $GroupName = $Group.DisplayName
+    Get-MsolGroupMember -GroupObjectId $Group.Objectid |select displayname, emailaddress, groupmembertype, islicensed|export-csv -append -path c:\temp\$CompanyName\$GroupName-Members.csv
+    write-output "All users in $GroupName" >>c:\temp\$CompanyName\$GroupName-Members.csv
+}
+write-host -foregroundcolor cyan "All reports are saved in c:\temp\$CompanyName\ directory."
