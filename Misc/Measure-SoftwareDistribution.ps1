2c399ce912f7f1fb2732a77bf20b580c0ea2f192
Cipher-Daemon <105097480+Cipher-Daemon@users.noreply.github.com>
2023-01-17 11:45:43 -0600

    Update Measure-SoftwareDistribution.ps1

--- Misc/Measure-SoftwareDistribution.ps1
+++ Misc/Measure-SoftwareDistribution.ps1
@@ -2,6 +2,6 @@ while ($true){
 cls
 $a = (Get-ChildItem -Recurse c:\windows\softwaredistribution|Measure-Object -sum length).sum
 '{0:N0}' -f $a
-start-sleep -seconds 10
 (get-date).datetime
+start-sleep -seconds 10
 }
