75baa0833f5a425e3b19ea611b2167cfd113093d
Cipher-Daemon <105097480+Cipher-Daemon@users.noreply.github.com>
2023-03-11 22:56:26 -0600

    Delete Move-All-Licensed-Users-to-Security-Group.ps1

--- MS-Exchange-and-365/Move-All-Licensed-Users-to-Security-Group.ps1
+++
@@ -1,9 +0,0 @@
-$Users = Get-MsolUser -All | where {$_.isLicensed -eq $true}
-
-$GroupPrompt = read-host -prompt "Group Name?"
-
-$DUOGroupGUID = ((Get-MsolGroup|?{$_.displayname -eq $GroupPrompt}).objectid).guid
-
-foreach ($User in $users){
-    Add-MsolGroupMember -GroupObjectId $DUOGroupGUID -GroupMemberType User -GroupMemberObjectId $user.ObjectId
-}
