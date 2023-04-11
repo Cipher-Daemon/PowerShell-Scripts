42724bad4d0b83332f77fd51b38fd3da08ad3b72
Cipher-Daemon <105097480+Cipher-Daemon@users.noreply.github.com>
2023-04-11 13:06:05 -0500

    Update Get-Last-Logon-Time.ps1

--- On-Prem-AD/Get-Last-Logon-Time.ps1
+++ On-Prem-AD/Get-Last-Logon-Time.ps1
@@ -1,2 +1,2 @@
 $Username = read-host -prompt "Who is the user?"
-Get-ADUser -Identity $Username -Properties LastLogon | Select Name, @{Name='LastLogon';Expression={[DateTime]::FromFileTime($_.LastLogon)}}
+Get-ADUser -Identity $Username -Properties * | Select Name, @{Name='LastLogon';Expression={[DateTime]::FromFileTime($_.LastLogon)}},@{Name='lastLogonTimestamp';Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}}
