d39a2e59644710fbcd23e754dbdb766c6dded2d5
Cipher-Daemon <105097480+Cipher-Daemon@users.noreply.github.com>
2023-10-03 11:38:52 -0500

    Update Base64-Decode.ps1

--- Misc/Base64-Decode.ps1
+++ Misc/Base64-Decode.ps1
@@ -1,1 +1,11 @@
-$X = read-host -prompt "Base64"; [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String("$X"))
+Function Base64-All3 {
+write-host "Unicode:"; [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String("$X"))
+write-host " "
+write-host "ASCII:"; [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String("$X"))
+write-host " "
+write-host "UTF8:"; [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("$X"))
+}
+
+$X = read-host -prompt "Base64"
+
+Base64-All3
